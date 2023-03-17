9c41c68a7f29947d37b2bf1f798a1efc1b0ee634
Atharva <atharva.malik@education.nsw.gov.au>
2023-03-17 14:54:04 +1100

    add intents for greetings

--- train.py
+++ train.py
@@ -1,5 +1,17 @@
 import json
+import random
+import string
 
+import nltk
+import numpy as np
+from nltk.stem import WordNetLemmatizer
+
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+
+nltk.download("punkt")
+nltk.download("wordnet")
 data = json.load(open('data.json',))
 
 print(data)
