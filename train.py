16cfde667a6db433df0c44081ebefddd4270594a
Atharva <atharva.malik@education.nsw.gov.au>
2023-03-17 14:56:35 +1100

    more code

--- train.py
+++ train.py
@@ -12,6 +12,14 @@ from tensorflow.keras.layers import Dense, Dropout
 
 nltk.download("punkt")
 nltk.download("wordnet")
+
+# initializing lemmatizer to get stem of words
+lemmatizer = WordNetLemmatizer()
 data = json.load(open('data.json',))
 
+# Each list to create
+words = []
+classes = []
+doc_X = []
+doc_Y = []
 print(data)
